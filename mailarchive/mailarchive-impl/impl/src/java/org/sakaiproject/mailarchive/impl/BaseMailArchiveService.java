896da835b5cc3d4c84705c4131b82d2f8f876e24
Mireia <mcalzada@gmail.com>
2021-04-29 13:30:26 +0200

    CVON-321 Treure el patch SAK-41717 per evitar codificacions en arxiu de correu

--- mailarchive/mailarchive-impl/impl/src/java/org/sakaiproject/mailarchive/impl/BaseMailArchiveService.java
+++ mailarchive/mailarchive-impl/impl/src/java/org/sakaiproject/mailarchive/impl/BaseMailArchiveService.java
@@ -825,14 +825,11 @@ public abstract class BaseMailArchiveService extends BaseMessage implements Mail
 		public MailArchiveMessage addMailArchiveMessage(String subject, String fromAddress, Instant dateSent, List mailHeaders,
 				List attachments, String[] body) throws PermissionException
 		{
-			StringBuilder alertMsg = new StringBuilder();
-			final String cleanedHtml = FormattedText.processFormattedText(body[1], alertMsg);
-			final String cleanedText = FormattedText.encodeUnicode(body[0]);
 
 			MailArchiveMessageEdit edit = (MailArchiveMessageEdit) addMessage();
 			MailArchiveMessageHeaderEdit archiveHeaders = edit.getMailArchiveHeaderEdit();
-			edit.setBody(cleanedText);
-			edit.setHtmlBody(cleanedHtml);
+			edit.setBody(body[0]);
+			edit.setHtmlBody(body[1]);
 			archiveHeaders.replaceAttachments(attachments);
 			archiveHeaders.setSubject(subject);
 			archiveHeaders.setFromAddress(fromAddress);
